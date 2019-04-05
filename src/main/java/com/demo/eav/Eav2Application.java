cd9f2cafbef630ce54b8313f75d3b07d1db53ab3
lvallep <lvallep@lvalleppw71.indra.es>
2019-04-05 10:14:24 -0500

    first

---
+++ src/main/java/com/demo/eav/Eav2Application.java
@@ -0,0 +1,13 @@
+package com.demo.eav;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Eav2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Eav2Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/demo/eav/Eav2ApplicationTests.java
@@ -0,0 +1,16 @@
+package com.demo.eav;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class Eav2ApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
